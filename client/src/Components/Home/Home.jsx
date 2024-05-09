19a0bcd2ccbeb5f3424486f7e359458f314afeca
vaishnavi <vaishnavi22kahar@gmail.com>
2024-05-09 19:37:21 +0530

    server deployed on render

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -14,6 +14,7 @@ import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Grid from "@mui/material/Grid";
+const apiUrl = process.env.REACT_APP_API_URL;
 
 function Home() {
   const [questions, setQuestions] = useState([]);
@@ -26,7 +27,7 @@ function Home() {
   useEffect(() => {
     const fetchQuestions = async () => {
       try {
-        const response = await fetch("http://localhost:5000/question");
+        const response = await fetch(`${apiUrl}/question`);
         const data = await response.json();
         setQuestions(data);
       } catch (error) {
